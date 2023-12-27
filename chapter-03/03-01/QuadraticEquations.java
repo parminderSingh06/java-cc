2af82c045a02131fe173f26de4367f342cc2eb04
Parminder <parmindersingh1086@gmail.com>
2023-12-26 20:47:48 -0500

    Problem solved, no challenge was imposed.

--- chapter-03/03-01/QuadraticEquations.java
+++ chapter-03/03-01/QuadraticEquations.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class QuadraticEquations {
     public static void main(String[] args) {
-
+        System.out.println("Enter a, b, c: ");
+        Scanner input = new Scanner(System.in);
+        double a = input.nextDouble();
+        double b = input.nextDouble();
+        double c = input.nextDouble();
+        double discriminant = (Math.pow(b,2) - 4*a*c);
+        double root1 = ((((-1)*b) + Math.pow(discriminant, 0.5))/(2*a));
+        if(discriminant < 0){
+            System.out.println("The equation has no real roots");
+        } else if (discriminant == 0) {
+            System.out.println("The equation has one root: " + root1);
+        }else {
+            double root2 = ((((-1)*b) - Math.pow(discriminant, 0.5))/(2*a));
+            System.out.println("The equation has one root: " + root1 + " and " + root2);
+        }
     }
 }
