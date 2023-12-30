7318247e69508950d5db3a7171b29a2adcd06599
Parminder Singh <parmindersingh1086@gmail.com>
2023-12-30 17:22:54 -0500

    Problem solved, no challenges were imposed.

---
+++ chapter-05/05-09/TwoHighestScores.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class TwoHighestScores {
+    public static void main(String[] args) {
+        //Sets up variables and takes user input on number of students
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter number of students that took the exam: ");
+        int numberOfStudents = input.nextInt();
+        System.out.println("Enter student name: ");
+        String currentStudent = input.next();
+        String topStudent = currentStudent;
+        System.out.println("Enter student score: ");
+        double currentScore = input.nextDouble();
+        double topScore = currentScore;
+        //Loop through and replace top student if score is higher
+        for(int i=0;i<numberOfStudents-1;i++){
+            if(currentScore > topScore){
+                topStudent = currentStudent;
+                topScore = currentScore;
+            }
+            System.out.println("Enter student name: ");
+            currentStudent = input.next();
+            System.out.println("Enter student grade: ");
+            currentScore = input.nextDouble();
+
+        }
+        //Print answer to console
+        System.out.println("The student with the top score is " + topStudent + " his score is " + topScore);
+    }
+}
